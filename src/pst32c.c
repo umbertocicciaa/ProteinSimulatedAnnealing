991edf8d30014c54602001e8ed3fc048c63a1f82
umbertocicciaa <umbertocicciaa@icloud.com>
2025-01-07 10:52:16 +0100

    ottimizzata hydro energy

--- src/pst32c.c
+++ src/pst32c.c
@@ -79,6 +79,8 @@ typedef struct
 } params;
 
 extern void rama_energy_assembly(VECTOR phi, VECTOR psi, type *rama);
+extern void hydrophobic_energy_assembly(char *s, int *n, MATRIX coords, type *result);
+extern electrostatic_energy_assembly(char *s, int *n, MATRIX coords, type *result);
 
 void *get_block(int size, int elements)
 {
@@ -429,45 +431,6 @@ MATRIX backbone(char *s, int n, VECTOR phi, VECTOR psi)
 	return coords;
 }
 
-type hydrophobic_energy(char *s, int n, MATRIX coords)
-{
-	type energy = 0;
-	int i, j, k;
-	VECTOR coords_c_alpha_i = alloc_matrix(1, 4);
-	VECTOR coords_c_alpha_j = alloc_matrix(1, 4);
-
-	for (i = 0; i < n; i++)
-	{
-
-		int idx_i = i * 3 * 3 + 3;
-
-		coords_c_alpha_i[0] = coords[idx_i];
-		coords_c_alpha_i[1] = coords[idx_i + 1];
-		coords_c_alpha_i[2] = coords[idx_i + 2];
-		coords_c_alpha_i[3] = 0;
-
-		for (j = i + 1; j < n; j++)
-		{
-
-			int idx_j = j * 3 * 3 + 3;
-
-			coords_c_alpha_j[0] = coords[idx_j];
-			coords_c_alpha_j[1] = coords[idx_j + 1];
-			coords_c_alpha_j[2] = coords[idx_j + 2];
-			coords_c_alpha_j[3] = 0;
-
-			type dist = sqrtf((coords_c_alpha_j[0] - coords_c_alpha_i[0]) * (coords_c_alpha_j[0] - coords_c_alpha_i[0]) + (coords_c_alpha_j[1] - coords_c_alpha_i[1]) * (coords_c_alpha_j[1] - coords_c_alpha_i[1]) + (coords_c_alpha_j[2] - coords_c_alpha_i[2]) * (coords_c_alpha_j[2] - coords_c_alpha_i[2]));
-
-			int pos_i = s[i] - 65;
-			int pos_j = s[j] - 65;
-
-			if (dist < 10.0)
-				energy = energy + (hydrophobicity[pos_i] * hydrophobicity[pos_j]) / dist;
-		}
-	}
-	return energy;
-}
-
 type electrostatic_energy(char *s, int n, MATRIX coords)
 {
 	type energy = 0;
@@ -548,6 +511,7 @@ type packing_energy(char *s, int n, MATRIX coords)
 	return energy;
 }
 
+
 type energy(char *s, int n, VECTOR phi, VECTOR psi)
 {
 	MATRIX coords = backbone(s, n, phi, psi);
@@ -555,8 +519,15 @@ type energy(char *s, int n, VECTOR phi, VECTOR psi)
 	type rama;
 	rama_energy_assembly(phi, psi, &rama);
 
-	type hydro = hydrophobic_energy(s, n, coords);
+	type hydro;
+	hydrophobic_energy_assembly(s, &n, coords, &hydro);
+    
 	type elec = electrostatic_energy(s, n, coords);
+	
+    //type elec;
+	//electrostatic_energy_assembly(s, &n, coords, &elec);
+	
+	
 	type pack = packing_energy(s, n, coords);
 
 	VECTOR v1 = alloc_matrix(1, 4);
--- src/pst32c_ASSEMBLY.nasm
+++ src/pst32c_ASSEMBLY.nasm
@@ -37,6 +37,9 @@ section .data
     beta_psi dd 113.0
     half dd 0.5
     
+    hydrophobicity dd 1.8, -1, 2.5, -3.5, -3.5, 2.8, -0.4, -3.2, 4.5, -1, -3.9, 3.8, 1.9, -3.5, -1, -1.6, -3.5, -4.5, -0.8, -0.7, -1, 4.2, -0.9, -1, -1.3, -1
+
+
 section .bss			
 	alignb 16
 	e		resd		1
@@ -120,6 +123,86 @@ rama_energy_assembly:
         jl .loop
 
     movss [ecx], xmm0
+
+    pop edi
+    pop esi
+    pop ebx
+    mov esp, ebp
+    pop ebp
+    ret
+
+global hydrophobic_energy_assembly
+
+hydrophobic_energy_assembly:
+    push ebp
+    mov ebp, esp
+    push ebx
+    push esi
+    push edi
+
+    mov eax, [ebp + 8]  ; s
+    mov ebx, [ebp + 12] ; n
+    mov ecx, [ebp + 16] ; coords
+    mov edx, [ebp + 20] ; result
+
+    xorps xmm0, xmm0    ; energy = 0
+
+    mov esi, 0          ; i = 0
+    .outer_loop:
+        cmp esi, ebx
+        jge .end_outer_loop
+
+        mov edi, esi
+        add edi, 1      ; j = i + 1
+
+        mov eax, ecx
+        add eax, esi
+        shl eax, 2
+        add eax, ecx
+        movaps xmm1, [eax + 12] ; coords_c_alpha_i
+
+        .inner_loop:
+            cmp edi, ebx
+            jge .end_inner_loop
+
+            mov eax, ecx
+            add eax, edi
+            shl eax, 2
+            add eax, ecx
+            movaps xmm2, [eax + 12] ; coords_c_alpha_j
+
+            movaps xmm3, xmm2
+            subps xmm3, xmm1
+            mulps xmm3, xmm3
+            movhlps xmm4, xmm3
+            addps xmm3, xmm4
+            movaps xmm4, xmm3
+            shufps xmm4, xmm4, 1
+            addss xmm3, xmm4
+            sqrtss xmm3, xmm3                ; dist = sqrtf(...)
+
+            movzx edx, byte [eax + esi]
+            sub edx, 65
+            movss xmm4, [hydrophobicity + edx * 4]
+
+            movzx edx, byte [eax + edi]
+            sub edx, 65
+            movss xmm5, [hydrophobicity + edx * 4]
+
+            mulss xmm4, xmm5
+            divss xmm4, xmm3
+
+            addss xmm0, xmm4
+
+            add edi, 1
+            jmp .inner_loop
+
+        .end_inner_loop:
+        add esi, 1
+        jmp .outer_loop
+
+    .end_outer_loop:
+    movss [edx], xmm0
 
     pop edi
     pop esi
